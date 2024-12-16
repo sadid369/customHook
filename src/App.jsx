303be4645c7218fb77c8253a54f86934b5bfd9cc
sadid369 <sadid.jones@gmail.com>
2024-12-17 02:12:54 +0600

    week 9.1 @ 1:00

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import React from "react";
 import "./App.css";
 import axios from "axios";
+import useSWR from "swr";
 
 function useTodos(n) {
   const [todos, setTodos] = useState([]);
@@ -28,21 +29,43 @@ function useTodos(n) {
   return { todos, loading };
 }
 function App() {
-  const { todos, loading } = useTodos(5);
+  // const { todos, loading } = useTodos(5);
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/todos",
+    (url) => axios.get(url).then((res) => res.data)
+  );
+
   return (
     <>
-      {loading ? (
+      {error ? (
+        "An error has occurred."
+      ) : isLoading ? (
         <p>Loading...</p>
       ) : (
-        todos.map((todo) => {
-          return <Todos todos={todo} />;
-        })
+        data.map((todo) => <Todos key={todo.id} todos={todo} />)
       )}
     </>
   );
+  // const { data, error, loading } = useSWR(
+  //   "https://api.github.com/repos/vercel/swr",
+  //   (url) => axios.get(url).then((res) => res.data)
+  // );
+
+  // if (error) return "An error has occurred.";
+  // if (loading) return "Loading...";
+  // return (
+  //   <div>
+  //     <h1>{data.name}</h1>
+  //     <p>{data.description}</p>
+  //     <strong>👁 {data.subscribers_count}</strong>{" "}
+  //     <strong>✨ {data.stargazers_count}</strong>{" "}
+  //     <strong>🍴 {data.forks_count}</strong>
+  //   </div>
+  // );
 }
 
 function Todos({ todos }) {
+  console.log(todos);
   return (
     <div key={todos.id}>
       <p>{todos.title}</p>
